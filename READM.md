cc53cdb87c4db1891ad2ed4650813aaf374019cb
Rafael Alves <rbalves.ads@gmail.com>
2020-04-30 19:10:14 -0400

    Initial commit

---
+++ READM.md
@@ -0,0 +1,5 @@
+# Projeto de Web Scraping 
+
+**Em andamento**
+
+Projeto do desafio proposto neste [artigo.](https://sejaumdatascientist.com/o-projeto-de-data-engineering-para-o-seu-portfolio/)
---
+++ wsbooks.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+
+url = 'http://books.toscrape.com/'
+
+options = Options()
+options.headless = True
+
+driver = webdriver.Firefox(options=options)
+driver.get(url)
+
+ul = driver\
+    .find_element_by_xpath('//*[@id="default"]/div/div/div/aside/div[2]/ul/li/ul')\
+    .find_elements_by_tag_name('li')
+
+categories = []
+
+for li in ul:
+    categories.append(li.text)
+
+print(categories)
+print(driver.capabilities)
